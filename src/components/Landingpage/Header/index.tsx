e800b1456b486aec6c1af5b87cd3c546631aed31
mvspavan001 <mvspavan001@gmail.com>
2025-03-12 11:59:58 +0530

    Responsive changes

--- src/components/Landingpage/Header/index.tsx
+++ src/components/Landingpage/Header/index.tsx
@@ -4,7 +4,7 @@ import plane4 from "@/assets/plane4.png";
 import Menu from "./menu";
 export default function Header() {
   return (
-    <div className="h-full w-full p-4 flex flex-col md:flex-row md:justify-evenly md:tems-center">
+    <div className="h-full w-full p-4 flex flex-col md:flex-row md:justify-evenly items-center">
       <div className="flex flex-row items-center justify-center md:justify-normal gap-x-4">
         <img src={plane4} className="w-12 h-12"/>
         <p className="text-3xl font-bold">Eco Travels</p>
--- src/components/Landingpage/MainContainer/index.tsx
+++ src/components/Landingpage/MainContainer/index.tsx
@@ -1,22 +1,43 @@
 import mainImage from "@/assets/image2.jpg";
 import plane from "@/assets/plane1.webp";
 import { FlipWords } from "@/components/ui/flip-words";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
 
 export default function MainPageIndex() {
   const words = ["Experience", "Discover", "Enjoy", "Feel"];
+  const container = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: container,
+    offset: ["start end", "end start"],
+  });
+
+  const yMovement = useTransform(
+    scrollYProgress, 
+    [0, 0.5, 1], 
+    ["10%", "0%", "-10%"]
+  );
+  const scale = useTransform(scrollYProgress, [0, 0.5, 1], [1, 1.05, 1.1]);
+  const opacity = useTransform(scrollYProgress, [0, 0.8, 1], [1, 0.8, 0.6]);
+
   return (
-    <div className="h-[calc(100vh-6rem)] md:p-8 pt-4 relative overflow-hidden">
-      <div className="w-full h-full overflow-hidden" style={{ borderRadius: 80 }}>
-        <img src={mainImage} loading="lazy" className="w-full h-full object-cover light:mix-blend-multiply" />
-      </div>
+    <div ref={container} className="h-[calc(100vh-6rem)] md:p-0 relative overflow-hidden rounded-xl">
+      <motion.div
+        style={{
+          y: yMovement,
+          scale: scale,
+          opacity: opacity,
+        }}
+        className="w-full h-full overflow-hidden absolute inset-0"
+      >
+        <img src={mainImage} loading="lazy" className="w-full h-screen object-cover rounded-xl light:mix-blend-multiply" />
+      </motion.div>
 
       <div className="absolute left-8 top-48 m-auto md:left-48 md:top-48 flex flex-col gap-y-8 text-black select-none">
         <p>ELEVATE YOUR TRAVEL JOURNEY</p>
         <div className="transition-all duration-300 ease-out">
           <FlipWords words={words} className="text-5xl lg:text-8xl font-bold text-primary dark:text-primary" />
-          <p className="text-5xl lg:text-8xl font-bold text-black">
-            The magic of
-          </p>
+          <p className="text-5xl lg:text-8xl font-bold text-black">The magic of</p>
           <p className="text-5xl lg:text-8xl font-bold text-black">Flight!</p>
         </div>
         <div className="w-48">
